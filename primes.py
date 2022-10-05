eacfbe667c0f143995237197805d43485bfe3a36
k21062319 <muhammad.r.rahman@kcl.ac.uk>
2022-10-05 23:53:39 +0100

    Do not accept a 0 or negative parameter

--- primes.py
+++ primes.py
@@ -3,4 +3,24 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+
+    count = 0
+    subject = 2
+
+    if number_of_primes > 0:
+        while count < number_of_primes:
+            flag = False
+            for i in range(2, subject):
+                if (subject % i) == 0:
+                    subject += 1
+                    flag = True
+                    break
+
+            if flag == False:
+                list.append(subject)
+                subject += 1
+                count += 1
+
+        return list
+    else:
+        raise ValueError("That was not a valid number. Please enter a number greater than 0.")
